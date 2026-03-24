4e05a70aee17bdd7dd1d55166cb7c62d84ae617a
Ananta Raihan <anantaraihan19@gmail.com>
2026-03-25 04:38:24 +0700

    cleanup: Remove all maintenance scripts and temporary files from repository

--- final_patch.php
+++
@@ -1,9 +0,0 @@
-<?php
-$filePath = $argv[1];
-$c = file_get_contents($filePath);
-
-// Langkah Pamungkas: Sikat sisa )) dan }); gaya lama sampe bersih
-$c = preg_replace('/\s*\}\)\);\s*\}\);\s*<\/script>/s', "\n    };\n</script>", $c);
-
-file_put_contents($filePath, $c);
-echo "Frontend KILAT 2.0 SUCCESS!\n";
